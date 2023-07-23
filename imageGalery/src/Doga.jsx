73928c4307fb52aba59d9524e1ea38d1e39a3b0c
Selim Zengin <selimzzz321@hotmail.com>
2023-07-23 14:34:47 +0300

    Manzara bölümü bitti

--- imageGalery/src/Doga.jsx
+++ imageGalery/src/Doga.jsx
@@ -5,19 +5,62 @@ export const Doga = () => {
   return (
  <>
  <br />
- <br />
- <br />
- <br />
- <br />
 
- <div className="container-fluid text-center" >
-  <div className="row row-cols-1 row-cols-lg-2 row-cols-xl-4 m-0">
-    <div className="col"><img src="https://media.istockphoto.com/id/1396812211/tr/foto%C4%9Fraf/photographer-traveler-taking-photo-of-the-beautiful-lake-at-sunset.webp?b=1&s=612x612&w=0&k=20&c=f6XjUQ9s2fXcNQ0V-s1boaS50NrQ1v4U4bHmJlcD7gQ=" alt="" /></div>
-    <div className="col"><img src="https://media.istockphoto.com/id/1396812211/tr/foto%C4%9Fraf/photographer-traveler-taking-photo-of-the-beautiful-lake-at-sunset.webp?b=1&s=612x612&w=0&k=20&c=f6XjUQ9s2fXcNQ0V-s1boaS50NrQ1v4U4bHmJlcD7gQ=" alt="" /></div>
-    <div className="col"><img src="https://media.istockphoto.com/id/1396812211/tr/foto%C4%9Fraf/photographer-traveler-taking-photo-of-the-beautiful-lake-at-sunset.webp?b=1&s=612x612&w=0&k=20&c=f6XjUQ9s2fXcNQ0V-s1boaS50NrQ1v4U4bHmJlcD7gQ=" alt="" /></div>
-    <div className="col"><img src="https://media.istockphoto.com/id/1396812211/tr/foto%C4%9Fraf/photographer-traveler-taking-photo-of-the-beautiful-lake-at-sunset.webp?b=1&s=612x612&w=0&k=20&c=f6XjUQ9s2fXcNQ0V-s1boaS50NrQ1v4U4bHmJlcD7gQ=" alt="" /></div>
-  </div>
+<h1 style={{marginLeft:50}}>24 doğa resim ve fotoğraf tamamen ücretsiz</h1> 
+<br />
+<h3 style={{marginLeft:50}}>Birbirinden güzel manzara ve resimleri buradan bulabilirsiniz</h3>
+<br />
+
+
+<div className='row'>
+    
+<div className='columun'>
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830_640.jpg" alt="" />
 </div>
+
+<div className='columun'>
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_640.jpg" alt="" />
+</div>
+
+<div  className='columun' >
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2016/05/24/16/48/mountains-1412683_640.png" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2016/01/08/11/57/butterflies-1127666_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_640.jpg" alt="" />
+</div>
+<div  className='columun' >
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2016/02/13/12/26/aurora-1197753_640.jpg" alt="" />
+</div>
+<div className='columun'>
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2017/01/14/12/59/iceland-1979445_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2016/04/18/22/05/seashells-1337565_640.jpg" alt="" />
+</div>
+
+<div className='columun'>
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2018/02/08/22/27/flower-3140492_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072_640.jpg" alt="" />
+</div>
+
+<div  className='columun' >
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2015/04/23/22/01/mountains-736886_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2018/08/23/07/35/thunderstorm-3625405_640.jpg" alt="" />
+</div>
+<div  className='columun' >
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2017/05/09/03/46/alberta-2297204_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2017/07/24/19/57/tiger-2535888_640.jpg" alt="" />
+<img style={{width:400,height:600}} src="https://cdn.pixabay.com/photo/2016/09/21/04/46/barley-field-1684052_640.jpg" alt="" />
+</div>
+</div>
+<br />
+<br />
 
 
 
